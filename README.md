6fbe9ed08a75395b8d8e7c2f9740713ac2e4b464
hawkrdg <cliff@hawkrdg.com>
2023-12-03 14:17:59 -0600

    major refactor to minimum display with animated controls, updated README...

--- README.md
+++ README.md
@@ -2,18 +2,40 @@
 
 ## An Angular / Material app to test interfacing to OpenAI's API.
 
-![the app...](src/assets/aidemo.png)
+![the app...](src/assets/aidemo1.png)
+
+**This is a major refactoring of the original project. Full parameter controls added for text completions. Images are returned as a 512x512 pixel URL. Completion object properties are displayed. The interface has been cleaned up in a minimalized way.**
 
 **AI... Whatever you may thnk of AI, it is the latest buzz in tech. As professional developers we really need to have at least a cursory idea about what AI is. One way to explore this is to incorporate OpenAI's API into our code. We can then use it in our own apps. This is a great way to learn some of the possibilites of AI without having to write our own engines. It's pretty easy: open an API account, generate some API keys, npm install openai, start writing code. Learning to talk to OpenAI so as to get exactly the output you want is not simple - it involves learning how to tune your prompts, provide examples, choose the correct output model, and more...**
 
 **This app is a simple gateway into exploring these ideas. The OpenAI API is promise-based so it works very well with 'async / await in javascript or typescript. Most everything you need to get started is in the README.md inside of node_modules/openai/**
 
-**Usage:**
+## Usage:
 
 - clone the repo, then npm install
 - ng serve aidemo --open
 
-**This app is finalized for using my VPS server to obfuscate my API_KEY. The code for testing within an Angular app without having to setup a separate server is included, but commented out. You basically run the code your server would run locally:**
+### Setting Parameters:
+
+![the app...](src/assets/aidemo2.png)
+
+**Here is where you enter your unlock key and experiment with various parameters for a text completion. Click 'Finished' when you get everything the way you want.**
+
+### A Text Completion:
+
+![the app...](src/assets/aidemo3.png)
+
+**This is a completion for the prompt: "in 50 words or less as a numbered list how to do the chicken dance'.**
+**The response gives the date, completion type, finish_reason ('stop' is OK, 'length' is not enough tokens), and token usage."**
+
+### An Image Completion:
+
+![the app...](src/assets/aidemo4.png)
+
+**This is an image for the prompt: ""still life with a pewter pitcher and pewter goblet on a small table in the style of Braque.**
+
+
+**This app is built for using my VPS server to obfuscate my API_KEY. The code for testing within an Angular app without having to setup a separate server is included, but commented out. You basically just swap the comments to run locally and make sure your API obj has the 'dangerouslyAllowBrowser' flag:**
 
 ```
 //-- include the api...
@@ -32,7 +54,7 @@ const requestBody = {
   prompt: this.imagePrompt,   // the prompt describing the image you want
   count: this.count,          // how many images to generate {default: 1}
   size: this.imageSize,       // one of '256x256' or '512x512' or '1024x1024'
-  format: this.imageFormat    // 'url' or 'base64_json`
+  format: this.imageFormat    // 'url' or 'base64_json`. URL's are transient.
 }
 
 const imageObj = await ai.images.generate(requestBody).then(
@@ -48,7 +70,7 @@ const imageUrl = imageObj.data[0].url;
 
 **It is unsafe to instantiate the api in your app for production - your OPENAI_API_KEY is exposed to the world and sooner or later you may find all your tokens are vanishing... To fix this you should move your credentials and API calls to your server.**
 
-**I am using an express https server and I created an openai endpoint: https://mydomain/openai. I only accept a post to this endpoint and the caller must include a 'secret' key to make it difficult for anyone to just hit my endpoint and use my tokens. I include an env.js file:**
+**I am using an express https server and I created an openai endpoint: https://mydomain/openai. I only accept a post to this endpoint and the caller must include, at a minimum, a 'secret key' and a 'method'. The 'secret key' makes it difficult for anyone to just hit my endpoint and use my tokens. I include an env.js file:**
 
 ```
 const process = {
@@ -111,7 +133,7 @@ app.post('/openai', async (req, res) => {
           dataObj = {
             prompt: req.body.prompt,
             n: req.body.count || 1,
-            size: req.body.size || '256x256',
+            size: req.body.size || '512x512',
             response_format: req.body.format || 'url'
           }
                 
@@ -130,9 +152,21 @@ app.post('/openai', async (req, res) => {
         }
         break;
 
+      case 'getModels':
+        ai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+       	await ai.models.list().then(
+       	  data => {
+            response = data;
+          },
+          err => {
+            aiStatus = 405;
+            response = err;
+          }
+       	);
+      	break;
+
       //
-      // blah
-      // blah
+      // add some other methods
       //
 
       default:
@@ -151,6 +185,6 @@ app.post('/openai', async (req, res) => {
     .send(response);
 });
 ```
-**This creates a fairly large function, but the logic seems clear to me and it works. There are probably better ways to do this or maybe refactor the API calls into a separate file. Let me know if you have a better way or a different way of protecting your API_KEY.**
+**This creates a fairly large function, but the logic seems clear to me, it works, and is easily extensible and easy to maintain. There are probably better ways to do this or maybe refactor the API calls into a separate file. Let me know if you have a better way or a different way of protecting your API_KEY.**
 
 **This demo barely scratches the surface of OpenAI. Head over to their site for full API docs and tutorials:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Open AI](https://openai.com)**
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { trigger, state, style, transition } from '@angular/animations';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 import OpenAI from 'openai';
+import { animate } from '@angular/animations';
+
+import { cloneDeep } from "lodash-es";
 
 const httpHeaders = new HttpHeaders(
   {
@@ -12,30 +16,58 @@ const httpHeaders = new HttpHeaders(
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fade', [
+      state('visible', style({opacity: 1, 'z-index': 100, left: '20px'})),
+      state('hidden', style({opacity: 0, 'z-index': -1, left: 'calc(90vw - 400px)'})),
+      transition('visible => hidden', [
+        animate('0.6s ease-in-out'),
+      ]),
+      transition('hidden => visible', [
+        animate('0.6s ease-in-out'),
+      ])
+    ])
+  ]
 }) export class AppComponent {
   title             = 'OpenAI Simple Demo';
   requestURL        = 'https://hawkrdg.com/openai';
+  
+  pwType            = 'password';
   unlockKey         = '';
 
-  currentResult     = '';
+  currentResult     = {
+    text: '',
+    object: '',
+    id: '',
+    model: '',
+    finish_reason: '',
+    total_tokens: 0,
+    prompt_tokens: 0,
+    completion_tokens: 0
+  };
   
   textPrompt        = '';
   model             = 'text-davinci-003';
   temperature       = 0.8;
   frequency_penalty = 0.0;
   presence_penalty  = 0.0;
-  max_tokens        = 128;
+  max_tokens        = 48;
 
   imagePrompt       = '';
   imageURL          = '';
-  imageSize         = '256x256';
-  imageFormat       = 'url'
+  imageSize         = '512x512';
+  imageFormat       = ''
 
   count             = 1;
 
+  showParameters    = false;
+  showResults       = false;
   showProgress      = false;
 
+  completionCreated = null;
+
+  @ViewChild('txtUnlockKey') txtUnlockKey: ElementRef;
 
   constructor(
     private http: HttpClient
@@ -43,7 +75,23 @@ const httpHeaders = new HttpHeaders(
 
   getIdea = async () => {
     console.log('getIdea()...');
-    this.currentResult = '';
+    this.currentResult = {
+      text: '',
+      object: '',
+      id: '',
+      model: '',
+      finish_reason: '',
+      total_tokens: 0,
+      prompt_tokens: 0,
+      completion_tokens: 0
+    };
+
+    this.showParameters = false;
+    this.showResults = false;
+    this.completionCreated = null;
+    this.imageURL = '';
+    this.imageFormat = '';
+
     this.showProgress = true;
     const requestBody = {
       key: this.unlockKey,
@@ -64,10 +112,21 @@ const httpHeaders = new HttpHeaders(
     await firstValueFrom(this.http.post(this.requestURL, requestBody,
                           {observe: 'body', responseType: 'json'})).then(
       data => {
-        this.currentResult = JSON.stringify(data, null, 2);
-      },
+        this.showResults = true;
+        this.currentResult.object = (data as any).object;
+        this.currentResult.id = (data as any).id;
+        this.currentResult.model = (data as any).model;
+        this.currentResult.finish_reason = (data as any).choices[0].finish_reason;
+        this.currentResult.total_tokens = (data as any).usage.total_tokens;
+        this.currentResult.prompt_tokens = (data as any).usage.prompt_tokens;
+        this.currentResult.completion_tokens = (data as any).usage.completion_tokens;
+        this.currentResult.text = (data as any).choices[0].text
+        const date = new Date();
+        date.setTime((data as any).created * 1000);
+        this.completionCreated = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    },
       err => {
-        this.currentResult = JSON.stringify(err.error, null, 2);
+        this.currentResult.text = JSON.stringify(err.error, null, 2);
       }
     );
     this.showProgress = false;
@@ -75,13 +134,28 @@ const httpHeaders = new HttpHeaders(
 
   getImage = async () => {
     console.log('getImage()...');
+    
+    this.currentResult = {
+      text: '',
+      object: '',
+      id: '',
+      model: '',
+      finish_reason: '',
+      total_tokens: 0,
+      prompt_tokens: 0,
+      completion_tokens: 0
+    };
+    this.completionCreated = null;
+    this.showParameters = false;
+    this.showResults = false;
     this.imageURL = '';
-    this.currentResult = '';
+    this.imageFormat = 'url';
+    
     this.showProgress = true;
     const requestBody = {
       key: this.unlockKey,
       method: 'getImage',
-      prompt: this.imagePrompt,
+      prompt: this.textPrompt,
       count: this.count,
       size: this.imageSize,
       format: this.imageFormat
@@ -94,13 +168,17 @@ const httpHeaders = new HttpHeaders(
     await firstValueFrom(this.http.post(this.requestURL, requestBody,
                           {observe: 'body', responseType: 'json'})).then(
       data => {
-        this.currentResult = JSON.stringify(data, null, 2);
         if ((data as any).data) {
+          this.showResults = true;
           this.imageURL = (data as any).data[0].url;
+          this.currentResult.object = 'image';
+          const date = new Date();
+          date.setTime((data as any).created * 1000);
+          this.completionCreated = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
         }
       },
       err => {
-        this.currentResult = JSON.stringify(err.error, null, 2);
+        this.currentResult.text = JSON.stringify(err.error, null, 2);
       }
     );
     this.showProgress = false;
@@ -108,7 +186,20 @@ const httpHeaders = new HttpHeaders(
 
   getModels = async () => {
     console.log('getModels()...');
-    this.currentResult = '';
+    this.currentResult = {
+      text: '',
+      object: '',
+      id: '',
+      model: '',
+      finish_reason: '',
+      total_tokens: 0,
+      prompt_tokens: 0,
+      completion_tokens: 0
+    };
+    this.imageURL = '';
+    this.imageFormat = ''
+    this.showParameters = false;
+    this.showResults = false;
     this.showProgress = true;
     const requestBody = {key: this.unlockKey, method: 'getModels'}
 
@@ -119,10 +210,14 @@ const httpHeaders = new HttpHeaders(
     await firstValueFrom(this.http.post(this.requestURL, requestBody, 
                           {observe: 'body', responseType: 'json'})).then(
       data => {
-        this.currentResult = JSON.stringify((data as any).data.map(r => r.id), null, 2); 
+        this.showResults = true;
+        this.currentResult.object = (data as any).object;
+        this.currentResult.text = JSON.stringify((data as any).data.map(r => r.id), null, 2); 
+        const date = new Date();
+        this.completionCreated = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
       },
       err => {
-        this.currentResult = JSON.stringify(err.error, null, 2);
+        this.currentResult.text = JSON.stringify(err.error, null, 2);
       }
     );
     this.showProgress = false;
